670596e7f4c230214191bf0463f6e492f6ce5ba0
Bekzat Ashke <bekzatashken@gmail.com>
2021-05-29 08:53:32 +0600

    feat: axios, fix: styles

--- src/components/Temp/Temp.jsx
+++ src/components/Temp/Temp.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import axios  from 'axios'
+import React, { useState, useEffect } from 'react'
 import { AQI } from './AQI/AQI'
 import { IoIosArrowForward } from 'react-icons/io'
-import { WiMoonAltFull } from 'react-icons/wi'
 import { Link } from 'react-router-dom'
 import './style.sass'
 
 export const Temp = () => {
+
+  const [ AppState, setAppState ] = useState(``)
+ 
+  useEffect(() => {
+    const id = `cf0328590b7fd3dec8b9bb899f788ffc`
+    const city = `los angeles`
+    const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=los angeles&appid=cf0328590b7fd3dec8b9bb899f788ffc`;
+    axios.get(apiUrl).then((resp) => {
+      const allPersons = resp.data;
+      setAppState(allPersons);
+    }).catch(error => console.log(error));
+  }, [setAppState]);
+
+
   return (
     <div className='temp' >
       <div className='temp-title-block' >
-        <p className='temp-title' >29<sup>&deg;</sup></p>
+        <p className='temp-title' >{`12`}<sup>&deg;</sup></p>
       </div>
       <p className='temp-footer' >Ясно</p>
       <AQI />
